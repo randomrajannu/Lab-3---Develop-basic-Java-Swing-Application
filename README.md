50586b7a4530077b01220cf3c4d220fb7fe9b185
randomrajannu <panchal.raj@northeastern.edu>
2023-02-08 10:51:30 -0500

    as

---
+++ README.md
@@ -0,0 +1,5 @@
+# INFO5100 lab assignment 1
+
+## lab-assignment-1
+
+This repo contains files for the 1st lab assignment for INFO5100 class
---
+++ src/dao/UsersDetails.java
@@ -0,0 +1,197 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author rajan
+ */
+public class UsersDetails {
+    private String firstName;
+    private String lastName;
+    private String dob;
+    private int age;
+    private long telNumber;
+    private String email;
+    private String addr1;
+    private String addr2;
+    private String city;
+    private String country;
+    private String affiliation;
+    private String major;
+    private String career;
+    private String degree1;
+    private String deg1Start;
+    private String deg1End;
+    private String degree2;
+    private String deg2Start;
+    private String deg2End;
+    private ImageIcon pic;
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setTelNumber(long telNumber) {
+        this.telNumber = telNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddr1(String addr1) {
+        this.addr1 = addr1;
+    }
+
+    public void setAddr2(String addr2) {
+        this.addr2 = addr2;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public void setDegree1(String degree1) {
+        this.degree1 = degree1;
+    }
+
+    public void setDeg1Start(String deg1Start) {
+        this.deg1Start = deg1Start;
+    }
+
+    public void setDeg1End(String deg1End) {
+        this.deg1End = deg1End;
+    }
+
+    public void setDegree2(String degree2) {
+        this.degree2 = degree2;
+    }
+
+    public void setDeg2Start(String deg2Start) {
+        this.deg2Start = deg2Start;
+    }
+
+    public void setDeg2End(String deg2End) {
+        this.deg2End = deg2End;
+    }
+
+    public void setPic(ImageIcon pic) {
+        this.pic = pic;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public long getTelNumber() {
+        return telNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddr1() {
+        return addr1;
+    }
+
+    public String getAddr2() {
+        return addr2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public String getDegree1() {
+        return degree1;
+    }
+
+    public String getDeg1Start() {
+        return deg1Start;
+    }
+
+    public String getDeg1End() {
+        return deg1End;
+    }
+
+    public String getDegree2() {
+        return degree2;
+    }
+
+    public String getDeg2Start() {
+        return deg2Start;
+    }
+
+    public String getDeg2End() {
+        return deg2End;
+    }
+
+    public ImageIcon getPic() {
+        return pic;
+    }
+    
+    
+    
+}
